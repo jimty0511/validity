b0b63ffa204b978c1aa5e392aa5da34992c4f81c
Yu Tang <ty90511@gmail.com>
2018-09-01 10:44:37 -0400

    Initial commit

---
+++ src/main/java/coding/exercise/validity/ValidityApplication.java
@@ -0,0 +1,12 @@
+package coding.exercise.validity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidityApplication.class, args);
+	}
+}
